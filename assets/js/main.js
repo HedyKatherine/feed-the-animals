892cd1236f454c581079bb220561ad9fef1531e5
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-04-14 14:21:15 +0200

    change double to simple quotes in main file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
 // our first object
 let cat = {
-  name = "Kitty";
-  mood = "happy";
+  name = 'Kitty';
+  mood = 'happy';
   weight = 1;
-  status = "neutral";
+  status = 'neutral';
 }
 // our second object
 let dog = {
-  name = "Doggy";
-  mood = "anxious";
+  name = 'Doggy';
+  mood = 'anxious';
   weight = 11;
-  status = "neutral";
+  status = 'neutral';
 }
 function setNewImage() {
-  document.getElementById('image1').src ="assets/img/image3.png";
+  document.getElementById('image1').src ='assets/img/image3.png';
 }
 function setOldImage() {
-  document.getElementById('image1').src ="assets/img/image1.png";
+  document.getElementById('image1').src ='assets/img/image1.png';
 }
