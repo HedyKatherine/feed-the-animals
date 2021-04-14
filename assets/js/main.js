7bc0d925581ac70da1cb3facfa7f8f809e45a8b3
olga <>
2021-04-14 14:09:05 +0200

    img changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,18 @@ let cat = {
   name = "Kitty";
   mood = "happy";
   weight = 1;
-  status = "hungry";
+  status = "neutral";
 }
 // our second object
 let dog = {
   name = "Doggy";
   mood = "anxious";
   weight = 11;
-  status = "full";
+  status = "neutral";
 }
 function setNewImage() {
   document.getElementById('image1').src ="assets/img/image3.png"
 }
 function setOldImage() {
+  document.getElementById('image1').src ="assets/img/image1.png"
 }
