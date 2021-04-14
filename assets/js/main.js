1ac232acc16cef389b011514fd7d06844a8fd046
olga <>
2021-04-14 16:23:17 +0200

    our first if/else statment function; some images added

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,3 +25,23 @@ function displayFood(){
 function displayActivities(){
   document.getElementById('playWithThem').style.visibility = 'visible';
 }
+// changer l'humeur du chaton
+function moodReaction(paramètreMood){
+if (paramètreMood == 'happy'){
+  document.getElementById('image1').src ='assets/img/image1.png';
+  document.getElementById('moodText').innerHTML = 'Yummy ! One more please!';
+  document.getElementById('score').innerHTML = 50;
+}else if(paramètreMood == 'furious'){
+  document.getElementById('image1').src ='assets/img/image2.png';
+  document.getElementById('moodText').innerHTML = 'Disgusting ! Eat it by yourself !';
+  document.getElementById('score').innerHTML = 10;
+}else if(paramètreMood == 'moody'){
+  document.getElementById('image1').src ='assets/img/image5.png';
+  document.getElementById('moodText').innerHTML = 'You\'re so boring !';
+  document.getElementById('score').innerHTML = 20;
+}else if(paramètreMood == 'excited'){
+  document.getElementById('image1').src ='assets/img/image4.png';
+  document.getElementById('moodText').innerHTML = 'Go ahead ! Continue !';
+  document.getElementById('score').innerHTML = 40;
+}
+}
