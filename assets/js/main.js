2d5aa9a8a068458369d40cf3d46a77fdfb1fa49c
KASABITI <aggieksbt@gmail.com>
2021-04-14 14:53:01 +0200

    Image removing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 // our first object
 let cat = {
-  name = 'Kitty';
-  mood = 'happy';
-  weight = 1;
-  status = 'neutral';
-}
+  name : 'Kitty',
+  mood : 'happy',
+  weight : 1,
+  status : 'neutral',
+};
 // our second object
 let dog = {
-  name = 'Doggy';
-  mood = 'anxious';
-  weight = 11;
-  status = 'neutral';
-}
+  name : 'Doggy',
+  mood : 'anxious',
+  weight : 11,
+  status : 'neutral',
+};
 function setNewImage() {
   document.getElementById('image1').src ='assets/img/image3.png';
 }
