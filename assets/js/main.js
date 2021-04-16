c8f8537a903ebdec82b02236addc883df94e7651
KASABITI <aggieksbt@gmail.com>
2021-04-16 19:17:38 +0200

    Delete canvas and countdown animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,10 @@ for (message of funnyMessages){
   console.log(message);
 }
 // afficher les icons de nurriture & activités
-function displayFoodAndActivity(){
-  document.getElementById('feedThem').style.visibility = 'visible';
-  document.getElementById('playWithThem').style.visibility = 'visible';
-}
+// function displayFoodAndActivity(){
+//   document.getElementById('feedThem').style.visibility = 'visible';
+//   document.getElementById('playWithThem').style.visibility = 'visible';
+// }
 function reactionMessages(source, message, moodBarScore, weightBarScore, statusBarScore, mood, weight, status){
   document.getElementById('image1').src = source;
   document.getElementById('moodText').innerHTML = message;
@@ -49,7 +49,7 @@ function moodReaction(parametreMood){
   }else if(parametreMood == 'moody'){
     reactionMessages('assets/img/image5.png', funnyMessages[2], -2, 1, 0, 'moody', 'neutral', 'hungry');
     cat.mood = 'moody';
-    cat.weight = "neutral";
+    cat.weight = 'neutral';
     cat.status = 'hungry';
     console.log(cat);
   }else if(parametreMood == 'excited'){
@@ -65,93 +65,119 @@ function moodReaction(parametreMood){
     cat.status = 'stuffed';
     console.log(cat);
   }
-insertInGlobalScore();
-insertInProgressionBar();
-insertInWeightBar();
-insertInStatusBar();
-moveMoodBar();
-moveWeightBar();
-moveStatusBar();
+  insertInGlobalScore();
+  insertInProgressionBar();
+  insertInWeightBar();
+  insertInStatusBar();
+  moveMoodBar();
+  moveWeightBar();
+  moveStatusBar();
 }
-
 // la fonction permettant de faire la bare de progression dynamique Humeur
 let progressionSum = 0;
 let progressionBarArray = [];
 function insertInProgressionBar(){
   let progressbarScore = parseInt(document.getElementById('progressbar-score').text);
-  document.getElementById('progressbar-score').innerHTML = progressbarScore;
+  // document.getElementById('progressbar-score').innerHTML = progressbarScore;
   progressionBarArray.push(progressbarScore);
   progressionSum += progressbarScore;
-  document.getElementById('progressSumScore').innerHTML = progressionSum;
+  // document.getElementById('progressSumScore').innerHTML = progressionSum;
 }
 // barre de progression weight
 let progressionWeightSum = 0;
 let progressionWeightArray = [];
 function insertInWeightBar(){
   weightbarScore = parseInt(document.getElementById('weightbar-score').text);
-  document.getElementById('weightbar-score').innerHTML = weightbarScore;
+  // document.getElementById('weightbar-score').innerHTML = weightbarScore;
   progressionWeightArray.push(weightbarScore);
   progressionWeightSum += weightbarScore;
-  document.getElementById('weightSumScore').innerHTML = progressionWeightSum;
+  // document.getElementById('weightSumScore').innerHTML = progressionWeightSum;
 }
 // barre de progression status
 let progressionStatusSum = 0;
 let progressionStatusArray = [];
 function insertInStatusBar(){
   statusbarScore = parseInt(document.getElementById('statusbar-score').text);
-  document.getElementById('statusbar-score').innerHTML = statusbarScore;
+  // document.getElementById('statusbar-score').innerHTML = statusbarScore;
   progressionStatusArray.push(statusbarScore);
   progressionStatusSum += statusbarScore;
-  document.getElementById('statusSumScore').innerHTML = progressionStatusSum;
+  // document.getElementById('statusSumScore').innerHTML = progressionStatusSum;
 }
-
-
-
-
-
-
 // new fonction to change weight of cat
-// function changeCatWeight(weightParameter) {
-//   let reaction;
-//   let weightScore;
-//   let image = document.getElementById('image1');
-//   switch (weightParameter) {
-//     case 'light'  :
-//       reaction = 'Yes ! I love to play !';
-//       weightScore = 50;
-//       cat.weight = 'light';
-//       console.log(cat);
-//       break;
-//     case 'medium' :
-//     reaction = 'I\'m so sleepy... zzz...';
-//     weightScore = 70;
-//     image.src = 'assets/img/image8.png';
-//     cat.weight = 'medium';
-//   }
-// COUNT DOUNT
-
-document.addEventListener('DOMContentLoaded', () =>{
-const timeLeftDisplay = document.querySelector('#time-left');
-const startBtn = document.querySelector('#start-button');
- let timeLeft = 90;
-
-function countDown(){
-  setInterval(function(){
-  if (timeLeft <= 0) {
-    clearInterval(timeLeft = 0);
+function changeCatWeight(weightParameter) {
+  let reaction;
+  let weightScore;
+  let image = document.getElementById('image1');
+  // switch (expression)
+  switch (weightParameter) {
+    // first case value
+    case 'light'  :
+    // first statement
+    reaction = 'Yes ! I love to play !';
+    weightScore = 50;
+    cat.mood = '';
+    cat.weight = 'light';
+    cat.status = '';
+    var sound = document.getElementById('meow');
+    sound.play();
+    console.log(cat);
+    // first break statement to halt first switch case
+    break;
+    case 'heavy' :
+    reaction = 'I\'m so sleepy...\nzzz...';
+    weightScore = 70;
+    image.src = 'assets/img/image8.png';
+    cat.mood = '';
+    cat.weight = 'heavy';
+    cat.status = '';
+    var sound = document.getElementById('meow');
+    sound.play();
+    console.log(cat);
+    break;
+    case 'medium' :
+    reaction = 'Do you want to dance\n?';
+    weightScore = 65;
+    image.src = 'assets/img/image10.png';
+    cat.mood = '';
+    cat.weight = 'medium';
+    cat.status = '';
+    var sound = document.getElementById('meow');
+    sound.play();
+    console.log(cat);
+    break;
+    case 'neutral' :
+    reaction = 'Not the bath\n!\nI can wash myself\n!';
+    weightScore = 60;
+    image.src = 'assets/img/image9.png';
+    cat.weight = 'neutral';
+    var sound = document.getElementById('roar');
+    sound.play();
+    console.log(cat);
+    break;
+    case 'chonk' :
+    reaction = 'Let\'s watch some T.V.,\njust you & me\n!';
+    weightScore = 80;
+    image.src = 'assets/img/image6.png';
+    cat.mood = '';
+    cat.weight = 'chonk';
+    cat.status = '';
+    var sound = document.getElementById('meow');
+    sound.play();
+    console.log(cat);
+    break;
+    default:
+    reaction = 'test';
+    weightScore = 0;
   }
-    timeLeftDisplay.innerHTML = timeLeft;
-    timeLeft -=1;
-  },1000);
+  document.getElementById('moodText').innerHTML = reaction;
+  document.getElementById('score').innerHTML = weightScore;
 }
-  startBtn.addEventListener('click', countDown );
-});
 // la nouvelle bar de progression l'Humeur
 var i = 0;
 function moveMoodBar() {
   if (i == 0) {
     i = 1;
-    var elem = document.getElementById("myBar");
+    var elem = document.getElementById('myBar');
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
@@ -164,14 +190,13 @@ function moveMoodBar() {
       }
     }
   }
-
 }
 // la nouvelle bar de progression le Poids
 var weightCount = 0;
 function moveWeightBar() {
   if (weightCount == 0) {
     weightCount = 1;
-    var weightElem = document.getElementById("weightBar");
+    var weightElem = document.getElementById('weightBar');
     var weightWidth = 1;
     var idWeight = setInterval(weightframe, 10);
     function weightframe() {
@@ -184,14 +209,13 @@ function moveWeightBar() {
       }
     }
   }
-
 }
 // la nouvelle bar de progression le Status
 var statusCount = 0;
 function moveStatusBar() {
   if (statusCount == 0) {
     statusCount = 1;
-    var statusElem = document.getElementById("statusBar");
+    var statusElem = document.getElementById('statusBar');
     var statusWidth = 1;
     var idStatus = setInterval(statusframe, 10);
     function statusframe() {
@@ -204,15 +228,96 @@ function moveStatusBar() {
       }
     }
   }
-
+}
+// // COUNTDOWN function
+// document.addEventListener('DOMContentLoaded', () =>{
+//   const timeLeftDisplay = document.querySelector('time-left');
+//   const startBtn = document.querySelector('start-button');
+//   let timeLeft = 90;
+//   function countDown(){
+//     setInterval(function(){
+//       if (timeLeft <= 0) {
+//         clearInterval(timeLeft = 0);
+//       }
+//       timeLeftDisplay.innerHTML = timeLeft;
+//       timeLeft -=1;
+//     },1000);
+//   }
+//   startBtn.addEventListener('click', countDown );
+// });
+// la nouvelle bar de progression l'Humeur
+var i = 0;
+function moveMoodBar() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById('myBar');
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= progressionSum) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+// la nouvelle bar de progression le Poids
+var weightCount = 0;
+function moveWeightBar() {
+  if (weightCount == 0) {
+    weightCount = 1;
+    var weightElem = document.getElementById('weightBar');
+    var weightWidth = 1;
+    var idWeight = setInterval(weightframe, 10);
+    function weightframe() {
+      if (weightWidth >= progressionWeightSum) {
+        clearInterval(idWeight);
+        weightCount = 0;
+      } else {
+        weightWidth++;
+        weightElem.style.width = weightWidth + "%";
+      }
+    }
+  }
+}
+// la nouvelle bar de progression le Status
+var statusCount = 0;
+function moveStatusBar() {
+  if (statusCount == 0) {
+    statusCount = 1;
+    var statusElem = document.getElementById('statusBar');
+    var statusWidth = 1;
+    var idStatus = setInterval(statusframe, 10);
+    function statusframe() {
+      if (statusWidth >= progressionStatusSum) {
+        clearInterval(idStatus);
+        statusCount = 0;
+      } else {
+        statusWidth++;
+        statusElem.style.width = statusWidth + "%";
+      }
+    }
+  }
 }
 // la partie du code permettant d'avoir la somme totale des points
 let sum = 0;
 let yourGlobalScore = [];
 function insertInGlobalScore(){
-  let sumOfTwoScores = progressionSum + progressionWeightSum + progressionStatusSum;
-  yourGlobalScore.push(sumOfTwoScores);
-  sum += sumOfTwoScores;
+  let weight = parseInt(document.getElementById('weightbar-score').text);
+  document.getElementById('weightbar-score').innerHTML = weight;
+  yourGlobalScore.push(weight);
+  let status = parseInt(document.getElementById('statusbar-score').text);
+  document.getElementById('statusbar-score').innerHTML = status;
+  yourGlobalScore.push(status);
+  let mood = parseInt(document.getElementById('progressbar-score').text);
+  document.getElementById('progressbar-score').innerHTML = mood;
+  progressionBarArray.push(mood);
+  let sumOfScores = status + weight + mood;
+  yourGlobalScore.push(sumOfScores);
+  sum += sumOfScores;
   document.getElementById('yourSumScore').innerHTML = sum;
-
+  document.getElementById('yourSumScore').style.backgroundImage = "url('assets/img/red-background.png')";
 }
