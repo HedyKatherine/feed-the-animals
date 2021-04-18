e8e5d22f85fec97cce301b80d40ad3baefd7cf16
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-04-18 19:23:49 +0200

    balance progress bars; modify switch case code blocks; add font

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,9 +17,10 @@ const funnyMessages = ['Yummy! One more please!', 'Disgusting! Eat it by yoursel
 for (message of funnyMessages){
   console.log(message);
 }
-function reactionMessages(source, message, moodBarScore, weightBarScore, statusBarScore){
+function reactionMessages(source, message, messageStyle, moodBarScore, weightBarScore, statusBarScore){
   document.getElementById('image1').src = source;
   document.getElementById('moodText').innerHTML = message;
+  document.getElementById('moodText').style.fontFamily = messageStyle;
   document.getElementById('progressbar-score').text = moodBarScore;
   document.getElementById('weightbar-score').text = weightBarScore;
   document.getElementById('statusbar-score').text = statusBarScore;
@@ -27,22 +28,23 @@ function reactionMessages(source, message, moodBarScore, weightBarScore, statusB
 // changer l'humeur du chaton
 function moodReaction(parametreMood){
   if (parametreMood == 'happy'){
-    reactionMessages('assets/img/image1.png', funnyMessages[0], 3, 6, 10);
+    reactionMessages('assets/img/image1.png', funnyMessages[0], 'Bubblegum sans', 3, 8, 10);
     document.getElementById('statusBar').innerHTML = cat.status;
     console.log(cat);
   }else if(parametreMood == 'furious'){
-    reactionMessages('assets/img/image7.png', funnyMessages[1], -3, -2, -9);
+    reactionMessages('assets/img/image7.png', funnyMessages[1], 'Creepster', -8, -2, -9);
     console.log(cat);
   }else if(parametreMood == 'moody'){
-    reactionMessages('assets/img/image5.png', funnyMessages[2], -2, 1, 0);
+    reactionMessages('assets/img/image5.png', funnyMessages[2], 'Fontdiner Swanky', -7, 1, 0);
     console.log(cat);
   }else if(parametreMood == 'excited'){
-    reactionMessages('assets/img/image4.png', funnyMessages[3], 4, 3.5, 9);
+    reactionMessages('assets/img/image4.png', funnyMessages[3], 'Luckiest Guy', 4, 5.5, 9);
     document.getElementById('statusBar').innerHTML = cat.status;
     console.log(cat);
-  }else if(parametreMood == 'excited-too-much'){
-    reactionMessages('assets/img/image14.png', funnyMessages[4], 5, 5.5, 10);
+  }else if(parametreMood == 'overexcited'){
+    reactionMessages('assets/img/image14.png', funnyMessages[4], 'Sniglet', 5, 10, 10);
     document.getElementById('statusBar').innerHTML = cat.status;
+    document.getElementById('moodText').style.fontWeight = '800';
     console.log(cat);
   }
   insertInGlobalScore();
@@ -52,6 +54,91 @@ function moodReaction(parametreMood){
   moveMoodBar();
   moveWeightBar();
   moveStatusBar();
+}
+// new fonction to change weight of cat
+function changeCatWeight(weightParameter) {
+  let reaction;
+  let reactionFontFamily;
+  let weightScore;
+  let image = document.getElementById('image1');
+  let moodScore;
+  let statusScore;
+  // switch (expression)
+  switch (weightParameter) {
+    // first case value
+    case 'light'  :
+    // first statement
+    reaction = 'Yes! I love to play!';
+    reactionFontFamily = 'Bubblegum sans';
+    weightScore = -5;
+    moodScore = 2;
+    statusScore = -0.5;
+    var sound = document.getElementById('meow');
+    sound.play();
+    console.log(cat);
+    // first break statement to halt first switch case
+    break;
+    case 'heavy' :
+    reaction = 'I\'m so sleepy...zzz...';
+    reactionFontFamily = 'Bubblegum sans';
+    weightScore = 3;
+    moodScore = 6;
+    statusScore = 3;
+    image.src = 'assets/img/image8.png';
+    var sound = document.getElementById('meow');
+    sound.play();
+    console.log(cat);
+    break;
+    case 'medium' :
+    reaction = 'Do you want to dance?';
+    reactionFontFamily = 'Bubblegum sans';
+    weightScore = -3;
+    moodScore = 2.5;
+    statusScore = -0.3;
+    image.src = 'assets/img/image10.png';
+    var sound = document.getElementById('meow');
+    sound.play();
+    console.log(cat);
+    break;
+    case 'neutral' :
+    reaction = 'Not the bath!\nI can wash myself!';
+    reactionFontFamily = 'Bubblegum sans';
+    weightScore = 0;
+    moodScore = -10;
+    statusScore = 0;
+    image.src = 'assets/img/image9.png';
+    var sound = document.getElementById('roar');
+    sound.play();
+    console.log(cat);
+    break;
+    case 'chonk' :
+    reaction = 'Let\'s watch some T.V.!';
+    reactionFontFamily = 'Bubblegum sans';
+    weightScore = 5;
+    moodScore = 5;
+    statusScore = 3.5;
+    image.src = 'assets/img/image6.png';
+    var sound = document.getElementById('meow');
+    sound.play();
+    console.log(cat);
+    break;
+    default:
+    reaction = 'test';
+    weightScore = 0;
+  }
+  document.getElementById('moodText').innerHTML = reaction;
+  document.getElementById('moodText').style.fontFamily = reactionFontFamily;
+  document.getElementById('weightbar-score').text = weightScore;
+  document.getElementById('progressbar-score').text = moodScore;
+  document.getElementById('statusbar-score').text = statusScore;
+//  document.getElementById('score').innerHTML = weightScore;
+insertInGlobalScore();
+insertInProgressionBar();
+insertInWeightBar();
+insertInStatusBar();
+moveMoodBar();
+moveWeightBar();
+moveStatusBar();
 }
 // la fonction permettant de faire la bare de progression dynamique Humeur
 let progressionSum = 0;
@@ -68,11 +155,11 @@ function insertInProgressionBar(){
       document.getElementById('myBar').style.background = 'red';
       cat.mood = "moody";
       document.getElementById('myBar').innerHTML = cat.mood;
-    }else if(progressionSum <= 5){
+    }else if(progressionSum <= 15){
       document.getElementById('myBar').style.background = '#4CAF50';
       cat.mood = "happy";
       document.getElementById('myBar').innerHTML = cat.mood;
-    }else if(progressionSum <= 15){
+    }else if(progressionSum <= 30){
       document.getElementById('myBar').style.background = '#4CAF50';
       cat.mood = "excited";
       document.getElementById('myBar').innerHTML = cat.mood;
@@ -91,19 +178,19 @@ function insertInWeightBar(){
   progressionWeightArray.push(weightbarScore);
   progressionWeightSum += weightbarScore;
   document.getElementById('weightSumScore').innerHTML = progressionWeightSum;
-    if(progressionWeightSum <= 3){
+    if(progressionWeightSum <= -3){
       document.getElementById('weightBar').style.background = 'red';
       cat.weight = "light";
       document.getElementById('weightBar').innerHTML = cat.weight;
-    }else if(progressionWeightSum <= 6){
+    }else if(progressionWeightSum <= 0){
       cat.weight = "normal";
       document.getElementById('weightBar').innerHTML = cat.weight;
       document.getElementById('weightBar').style.background = '#4CAF50';
-    }else if(progressionWeightSum <= 10){
+    }else if(progressionWeightSum <= 15){
       cat.weight = "medium";
       document.getElementById('weightBar').innerHTML = cat.weight;
       document.getElementById('weightBar').style.background = '#4CAF50';
-    }else if(progressionWeightSum <= 12){
+    }else if(progressionWeightSum <= 30){
       cat.weight = "heavy";
       document.getElementById('weightBar').innerHTML = cat.weight;
       document.getElementById('weightBar').style.background = 'red';
@@ -121,7 +208,6 @@ function insertInStatusBar(){
   // document.getElementById('statusbar-score').innerHTML = statusbarScore;
   progressionStatusArray.push(statusbarScore);
   progressionStatusSum += statusbarScore;
-  // document.getElementById('statusSumScore').innerHTML = progressionStatusSum;
     if(progressionStatusSum <= 3){
       document.getElementById('statusBar').style.background = 'red';
       cat.status = "starving";
@@ -140,80 +226,11 @@ function insertInStatusBar(){
       document.getElementById('statusBar').innerHTML = cat.status;
     }
 }
-// new fonction to change weight of cat
-function changeCatWeight(weightParameter) {
-  let reaction;
-  let weightScore;
-  let image = document.getElementById('image1');
-  // switch (expression)
-  switch (weightParameter) {
-    // first case value
-    case 'light'  :
-    // first statement
-    reaction = 'Yes ! I love to play !';
-    weightScore = 50;
-    cat.mood = '';
-    cat.weight = 'light';
-    cat.status = '';
-    var sound = document.getElementById('meow');
-    sound.play();
-    console.log(cat);
-    // first break statement to halt first switch case
-    break;
-    case 'heavy' :
-    reaction = 'I\'m so sleepy...\nzzz...';
-    weightScore = 70;
-    image.src = 'assets/img/image8.png';
-    cat.mood = '';
-    cat.weight = 'heavy';
-    cat.status = '';
-    var sound = document.getElementById('meow');
-    sound.play();
-    console.log(cat);
-    break;
-    case 'medium' :
-    reaction = 'Do you want to dance\n?';
-    weightScore = 65;
-    image.src = 'assets/img/image10.png';
-    cat.mood = '';
-    cat.weight = 'medium';
-    cat.status = '';
-    var sound = document.getElementById('meow');
-    sound.play();
-    console.log(cat);
-    break;
-    case 'neutral' :
-    reaction = 'Not the bath\n!\nI can wash myself\n!';
-    weightScore = 60;
-    image.src = 'assets/img/image9.png';
-    cat.weight = 'neutral';
-    var sound = document.getElementById('roar');
-    sound.play();
-    console.log(cat);
-    break;
-    case 'chonk' :
-    reaction = 'Let\'s watch some T.V.,\njust you & me\n!';
-    weightScore = 80;
-    image.src = 'assets/img/image6.png';
-    cat.mood = '';
-    cat.weight = 'chonk';
-    cat.status = '';
-    var sound = document.getElementById('meow');
-    sound.play();
-    console.log(cat);
-    break;
-    default:
-    reaction = 'test';
-    weightScore = 0;
-  }
-  document.getElementById('moodText').innerHTML = reaction;
-  document.getElementById('score').innerHTML = weightScore;
-}
 // COUNTDOWN function
 document.addEventListener('DOMContentLoaded', () =>{
   const timeLeftDisplay = document.getElementById('time-left');
   const startBtn = document.getElementById('start-button');
-  let timeLeft = 10;
+  let timeLeft = 30;
   function countDown(){
     setInterval(function(){
       if (timeLeft <= 0) {
